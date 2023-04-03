f126a050e18291962b3f4e1c51f7b609e782ec01
StoneyDSP <nathanjhood@googlemail.com>
2023-04-03 22:13:41 +0200

    Added a test 'cmodule' class

--- include/cmodule/cmodule.h
+++ include/cmodule/cmodule.h
@@ -3,6 +3,28 @@
 #ifndef INCLUDE_CMODULE_H_
 #define INCLUDE_CMODULE_H_
 
+// #include <v8.h>
+
+int main(int argc, char* argv[]);
+
+class cmodule
+{
+public:
+
+	cmodule();
+	~cmodule();
+
+	void setNumber(double val);
+
+	double getNumber();
+
+	double pow2(double val);
+
+	double numMulX(double val);
+
+private:
+	double number = {7.456};
+};
 
 
 #endif // INCLUDE_CMODULE_H_
--- src/cmodule.cc
+++ src/cmodule.cc
@@ -1,6 +1,37 @@
 #include "../include/cmodule/cmodule.h"
 
-int main()
+int main(int argc, char* argv[])
 {
   return 0;
 }
+
+
+
+cmodule::cmodule(): number(3.1459)
+{
+
+};
+cmodule::~cmodule()
+{
+
+};
+
+void cmodule::setNumber(double val)
+{
+	number = val;
+};
+
+/* Returns the number stored in the var */
+double cmodule::getNumber()
+{
+  return number;
+};
+
+double cmodule::pow2(double val)
+{
+  return (val * val);
+};
+double cmodule::numMulX(double val)
+{
+  return (number * val);
+};
