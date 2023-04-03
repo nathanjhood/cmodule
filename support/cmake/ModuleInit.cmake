e59d1274147d4b0b3e6284c048be93e6217a7f1c
StoneyDSP <nathanjhood@googlemail.com>
2023-04-03 22:04:08 +0200

    Minimalized external cmake helper file

--- support/cmake/ModuleInit.cmake
+++ support/cmake/ModuleInit.cmake
@@ -1,14 +1,5 @@
 #!/usr/bin/env cmake
 
-function(get_version)
-
-endfunction()
-
-function(cmake_defaults)
-
-	include(InstallRequiredSystemLibraries)
-
-endfunction()
 
 function(get_version)
 # Write CMake project version info to the cache, using external "VERSION" file
@@ -22,15 +13,4 @@ set(${PROJECT_NAME}_VERSION_FILE_PATCH ${CMAKE_MATCH_1} CACHE STRING "Project ve
 string(REGEX MATCH "[0-999].[0-999].[0-999].([0-999]*)" _ ${PROJECT_NAME}_VERSION_FILE)
 set(${PROJECT_NAME}_VERSION_FILE_TWEAK ${CMAKE_MATCH_1} CACHE STRING "Project version (tweak)")
 set(_ "")
-endfunction()
-
-
-
-
-function(add_headers VAR)
-set(headers ${${VAR}})
-foreach (header ${ARGN})
-	set(headers ${headers} include/${PROJECT_NAME}/${header})
-endforeach()
-set(${VAR} ${headers} PARENT_SCOPE)
 endfunction()
