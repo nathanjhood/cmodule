92ebc563a1c6dfa0f77e6f4c1a2a9e77c0f17cdb
StoneyDSP <nathanjhood@googlemail.com>
2023-04-29 08:27:13 +0200

    Reverted to CMake module for 'JoinPaths.cmake'

---
+++ support/cmake/Modules/JoinPaths.cmake
@@ -0,0 +1,15 @@
+# Copyright 2020 Jan Tojnar - MIT
+# https://github.com/jtojnar/cmake-snips
+function(join_paths joined_path first_path_segment)
+  set(temp_path "${first_path_segment}")
+  foreach(current_segment IN LISTS ARGN)
+    if (NOT ("${current_segment}" STREQUAL ""))
+      if (IS_ABSOLUTE "${current_segment}")
+        set(temp_path "${current_segment}")
+      else ()
+        set(temp_path "${temp_path}/${current_segment}")
+      endif ()
+    endif ()
+  endforeach()
+  set(${joined_path} "${temp_path}" PARENT_SCOPE)
+endfunction()
